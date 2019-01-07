b4c15a423a412d218215e66c5ea1d4fe11fe7880
crystalclear <oleg.crystalclear@gmail.com>
2019-01-07 21:15:42 +0200

    Implemented scripts

--- script.js
+++ script.js
@@ -2,4 +2,16 @@ var btn = document.querySelector('button'),
     modal = document.querySelector('.modal'),
     close = document.querySelector('.close');
 
+  btn.addEventListener('click', function() {
+    modal.style.display = 'flex';
+  });
 
+  close.addEventListener('click', function() {
+    modal.style.display = 'none';
+  });
+
+  window.addEventListener('click', function(e) {
+    if(e.target == modal) {
+      modal.style.display = 'none';
+    }
+  });
