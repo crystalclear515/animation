66f38f6c08c6c4f8c8a6f0a6e96cfef4b636922e
crystalclear <oleg.crystalclear@gmail.com>
2019-01-08 14:30:41 +0200

    Edits

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var btn = document.querySelector('button'),
-    modal = document.querySelector('.modal'),
-    close = document.querySelector('.close');
+  modal = document.querySelector('.modal'),
+  close = document.querySelector('.close');
 
-  btn.addEventListener('click', function() {
-    modal.style.display = 'flex';
-  });
+btn.addEventListener('click', function () {
+  modal.style.display = 'flex';
+});
 
-  close.addEventListener('click', function() {
+close.addEventListener('click', function () {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', function (e) {
+  if (e.target === modal) {
     modal.style.display = 'none';
-  });
-
-  window.addEventListener('click', function(e) {
-    if(e.target == modal) {
-      modal.style.display = 'none';
-    }
-  });
+  }
+});
